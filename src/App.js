d5cf12ea0cbaa1bd18f0532836b91561ce672594
Samson Maconi <git@samsonmaconi.com>
2020-03-31 03:53:14 -0700

    Chores

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ function App() {
   return (
     <div className="App">
       <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <h5 className="my-0 mr-md-auto font-weight-normal">Exoplanets Explorer</h5>
+        <h4 className="my-0 mr-md-auto font-weight-normal">Exoplanets Explorer <i class="fas fa-rocket"></i></h4>
       </div>
       <VirtualisedTable
         columns={columns}
